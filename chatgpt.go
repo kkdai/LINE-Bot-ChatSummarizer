e8fa89bb6f0816260f4ef529a7de8720cffa0a1a
Evan Lin <evan.if.lin@linecorp.com>
2022-12-30 19:48:59 +0800

    chagpt refine with doc

--- chatgpt.go
+++ chatgpt.go
@@ -8,17 +8,24 @@ import (
 )
 
 func CompleteContext(ori string) (ret string) {
+	// 取得 context
 	ctx := context.Background()
+
+	// 主要 API Open AI Completion https://beta.openai.com/docs/guides/completion
 	req := gpt3.CompletionRequest{
-		Model:     gpt3.GPT3TextDavinci003,
+		// Model: Davinci003 成果最好，但是也最慢。
+		Model: gpt3.GPT3TextDavinci003,
+		// 最大輸出內容，可以調整一下。
 		MaxTokens: 300,
-		Prompt:    ori,
+		// 輸入文字，也就是你平時在 ChatGPT 詢問他的問題。
+		Prompt: ori,
 	}
 	resp, err := client.CreateCompletion(ctx, req)
 	if err != nil {
 		ret = fmt.Sprintf("Err: %v", err)
 
 	} else {
+		// 回來的成果中，拿精準度最高的為答案。
 		ret = resp.Choices[0].Text
 	}
 	return ret
