1846ab11998efc61f28b617ca7da770c2380b8ad
Evan Lin <evan.if.lin@linecorp.com>
2023-01-11 22:52:12 +0800

    init pqsql

--- datappsql.go
+++ datappsql.go
@@ -1,19 +1,47 @@
 package main
 
+import (
+	"github.com/go-pg/pg/v10"
+	"github.com/go-pg/pg/v10/orm"
+)
+
 type PSqlDB struct {
-	db GroupStorage
+	Db *pg.DB
 }
 
 func (mdb *PSqlDB) ReadGroupInfo(roomID string) GroupData {
-	return mdb.db[roomID]
+	return GroupData{}
 }
 
 func (mdb *PSqlDB) AppendGroupInfo(roomID string, m MsgDetail) {
-	mdb.db[roomID] = append(mdb.db[roomID], m)
+	// mdb.db[roomID] = append(mdb.db[roomID], m)
 }
 
-func NewPQSql() *PSqlDB {
-	return &PSqlDB{
-		db: make(GroupStorage),
+func NewPQSql(url string) *PSqlDB {
+	options, _ := pg.ParseURL(url)
+	db := pg.Connect(options)
+
+	err := createSchema(db)
+	if err != nil {
+		panic(err)
 	}
+
+	return &PSqlDB{
+		Db: db,
+	}
+}
+
+func createSchema(db *pg.DB) error {
+	models := []interface{}{
+		(*GroupStorage)(nil),
+	}
+
+	for _, model := range models {
+		err := db.Model(model).CreateTable(&orm.CreateTableOptions{
+			IfNotExists: true})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
